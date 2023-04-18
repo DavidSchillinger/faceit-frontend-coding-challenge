ebaf7aecb706deb35cb33fef7b042676504d142b
davidschillinger <17913452+DavidSchillinger@users.noreply.github.com>
2023-04-18 11:19:42 +0000

    chore: separate tournaments page from root application setup
    Splits application setup and page rendering concerns.

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import GlobalStyle from './GlobalStyle';
 import store from './store';
-import Container from './components/Container';
-import H4 from './components/H4';
+import { Tournaments } from './pages/tournaments';
 
 const App = () => {
-  return (
-    <Container>
-      <H4>FACEIT Tournaments</H4>
-    </Container>
-  );
+  return <Tournaments />;
 };
 
 const container = document.getElementById('root');
---
+++ src/pages/tournaments/Tournaments.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '../../components/Container';
+import H4 from '../../components/H4';
+
+const Tournaments = () => (
+  <Container>
+    <H4>FACEIT Tournaments</H4>
+  </Container>
+);
+
+export default Tournaments;
